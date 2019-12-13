304b135aac4b6719a1d5bcf9ca0baa0e8b89862d
Damyan Peykov <dppeykov@gmail.com>
2019-12-13 10:50:40 +0100

    end of 68, section 5
    Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,23 @@ export class AppComponent {
     { type: "server", name: "TestServer", content: "Just a test" },
     { type: "server", name: "TestServer", content: "Just a test" }
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
