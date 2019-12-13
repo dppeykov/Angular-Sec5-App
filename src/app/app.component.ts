222006ff04adbf0400891d79aaa302ec0b98c34e
Damyan Peykov <dppeykov@gmail.com>
2019-12-13 09:54:21 +0100

    End of video 66 - section 5
    Binding to custom properties - we have the array of objects in the app component, then via the app template we are looping and passing the elements to the server-element component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "CodeSandbox";
+  serverElements = [
+    { type: "server", name: "TestServer", content: "Just a test" },
+    { type: "server", name: "TestServer", content: "Just a test" },
+    { type: "server", name: "TestServer", content: "Just a test" }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { ServerElementComponent } from "./server-element/server-element.component";
+import { CockpitComponent } from "./cockpit/cockpit.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ServerElementComponent, CockpitComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
